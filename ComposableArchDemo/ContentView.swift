14b05af555b0abb4076cbb5ab45be3cccdb65447
billidani7 <billidani7@yahoo.gr>
2020-05-15 15:45:18 +0300

    Initial Commit

---
+++ ComposableArchDemo/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ComposableArchDemo
+//
+//  Created by Vasilis Daningelis on 15/5/20.
+//  Copyright © 2020 Vasilis Daningelis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
